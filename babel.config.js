a23702c239f4b3a1db5f3b17c3ed5285f6b0aac5
uzigogetta <u.sangal@outlook.com>
2025-09-21 03:05:29 +0100

    build: use react-native-reanimated/plugin as last plugin (stabilize Android release animations)

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-worklets/plugin']
+    plugins: [
+      // Reanimated plugin MUST be last
+      'react-native-reanimated/plugin'
+    ],
   };
 };
